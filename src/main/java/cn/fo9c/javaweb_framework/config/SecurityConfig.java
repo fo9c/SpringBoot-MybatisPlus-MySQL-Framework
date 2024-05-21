a03f25b22e7ac02485171fb8d4c20dcde00fd36a
fo9c <1974555620@qq.com>
2024-05-21 12:12:26 +0800

    更新

--- src/main/java/cn/fo9c/javaweb_framework/config/SecurityConfig.java
+++ src/main/java/cn/fo9c/javaweb_framework/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package cn.fo9c.javaweb_framework.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
 // @EnableWebSecurity注解开启Spring Security的功能
@@ -22,27 +19,5 @@ public class SecurityConfig {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-
-
-//
-//    // 重写AuthenticationManager，用于认证
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.csrf(AbstractHttpConfigurer::disable)              // 关闭否则csrf,异域请求会被拦截
-//                .formLogin(form -> form
-//                        .loginPage("/login")                    // 自定义登录页面URL
-//                        .loginProcessingUrl("/login")           // 自定义登录处理URL
-//                        .successForwardUrl("/user/all")    // 登录成功后跳转URL
-//                        .failureForwardUrl("/user/all")    // 登录失败后跳转URL
-//
-//                );
-//
-//        // 构建过滤链并返回
-//        return http.build();
-//    }
-//
-
-
 
 }
